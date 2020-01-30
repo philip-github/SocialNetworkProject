cb43f04c62e36512aa8ecad962dc63c04893ce8f
philip-github <philiptwal93@gmail.com>
2020-01-30 09:49:47 -0600

    Initial Commit

---
+++ SocialNetworkProjectUpdated/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialNetworkProjectUpdated
+//
+//  Created by Philip Twal on 1/30/20.
+//  Copyright © 2020 Philip Twal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
